d7915ca0fdb39d48f26d3f6ae661bd41eff10274
brittany-erickson <bae@uoregon.edu>
2022-10-03 12:47:21 -0700

    add intro julia stuff

---
+++ documentation.jl
@@ -0,0 +1,20 @@
+"""
+   add_stuff(a, b)
+Compute and return the sum of numbers ‘a‘ and ‘b‘.
+# Examples
+‘‘‘jldoctest
+julia> x = 5; y = 7;
+julia> c = add_stuff(x, y)
+julia> 12
+‘‘‘
+"""
+function add_stuff(x, y)
+    
+    return x + y
+end
+
+
+A = rand(3,4)
+#B = 1
+#C = add_stuff(A, B)
+#print(C)
---
+++ intro_stuff.jl
@@ -0,0 +1,14 @@
+# this is julia code
+
+function add_stuff(x, y)
+    w = 5
+    return x + y
+end
+
+
+x = 5
+y = 3
+
+sum = add_stuff(x, y)
+7
+# add a comment
